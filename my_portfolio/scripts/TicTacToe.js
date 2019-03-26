c7437dfa469fb1481c6e09e11fc8bafab8a0c5f3
andrecas26 <andrew.castillo263@gmail.com>
2019-03-25 20:53:51 -0500

    tic tac toe

---
+++ my_portfolio/scripts/TicTacToe.js
@@ -0,0 +1,8 @@
+//1st person selects location
+//If location open place marker (error message if occupied)
+//computer selects marker
+//1st person selects marker(check for win or draw)
+//repeat till winner or draw
+//Option for new game
+<body onload="window.alert('Welcome to my home page!');">
+</body>
