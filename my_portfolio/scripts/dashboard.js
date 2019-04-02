17703756d7dccadd46ff1b95ff6c8d39e891bfa5
andrecas26 <andrew.castillo263@gmail.com>
2019-04-01 20:52:37 -0500

    changes

--- my_portfolio/scripts/dashboard.js
+++ my_portfolio/scripts/dashboard.js
@@ -1,3 +1,4 @@
+//Function of grocery list//
 $(function() {
 
     let $list = $('ul');
@@ -9,10 +10,23 @@ $(function() {
       $list.append(`<li>${text}</li>`);
       $('input[type="text"]').val('');
     });
-  
+//Function for location//  
     $list.on('click', 'li', function() {
       let $this = $(this);
       $this.remove();
     });
   
   });
+  var myLocation = document.getElementById("my-coordinates")
+
+function getLocation() {
+  if(navigator.geolocation){
+    navigator.geolocation.getCurrentPosition(displayPosition)
+  } else {
+   myLocation.innerHTML = "Not available"
+  }
+}
+
+function displayPosition(position) {
+  myLocation.innerHTML = "Longitude: " + position.coords.longitude + " Latitude: " + position.coords.latitude
+}
